230cdfc12287bbdf3e2360136135738cefa123e5
NonimUdara <nonimudara123@gmail.com>
2025-08-04 20:39:54 +0530

    create front end

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ProductCard from './components/productCard'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Nonim Udara</h1>
+      <ProductCard name="Apple iPad" price="$1299" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/medium/ipad_pro_e6b913694_2x.png" />
+      <ProductCard name="Apple iPhone" price="$899" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/medium/ipad_pro_e6b913694_2x.png"/>
+
+    </>
+  )
+}
+
+export default App
