8dbc68d9e72267f945d3aa772f6df649a8ccf919
NonimUdara <nonimudara123@gmail.com>
2025-08-11 19:31:44 +0530

    install tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>Nonim Udara</h1>
+      <h1 className='text-blue-400'>Nonim Udara</h1>
       <ProductCard name="Apple iPad" price="$1299" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/medium/ipad_pro_e6b913694_2x.png" />
       <ProductCard name="Apple iPhone" price="$899" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/medium/ipad_pro_e6b913694_2x.png"/>
 
