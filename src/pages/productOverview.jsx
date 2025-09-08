be778b5261ffa554717b313b9e11dc51a9dc427c
NonimUdara <nonimudara123@gmail.com>
2025-09-08 21:10:34 +0530

    Refactor ProductOverview component to improve product fetching logic and loading state handling; integrate Loader component for better user experience.

--- src/pages/productOverview.jsx
+++ src/pages/productOverview.jsx
@@ -1,15 +1,51 @@
-import { useLocation, useParams } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useLocation, useParams } from "react-router-dom";
+import { Loader } from "../components/loader";
 
-export default function ProductOverview () {
+export default function ProductOverview() {
+  const params = useParams();
+  //loading, success, error
+  const [status, setStaus] = useState("loading");
+  const [product, setProduct] = useState(null);
 
-    const params = useParams()
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_API_URL + "/api/products/" + params.id)
+      .then((res) => {
+        setProduct(res.data);
+        setStaus("success");
+      })
+      .catch(() => {
+        toast.error("Error fetching products");
+        setStaus("error");
+      });
+  }, []);
 
-    console.log(params)
+  console.log(params);
 
-    return (
-        <div>
-            product overview {params.id}
+  return(
 
-        </div>
-    )
+    <div>
+
+      {
+        status == "loading" && <Loader />
+      }
+      {
+        status == "success" && 
+        
+        (<div className="">
+
+
+
+        </div>)
+
+      }
+      {
+        status == "error" && <h1 className="text-red-500">Error</h1>
+      }
+
+    </div>
+  ) 
 }
