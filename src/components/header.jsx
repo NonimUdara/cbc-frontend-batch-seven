1a3562a93db95f62e9db9cc2628b0309a7261f8d
NonimUdara <nonimudara123@gmail.com>
2025-10-02 20:51:25 +0530

    update navbar with mobile responsiveness.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,19 +4,27 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="w-full bg-accent h-[100px] text-white px-[40px]">
-      <div className="w-full h-full flex relative">
+      <div className="w-full h-full flex items-center relative  ">
         <img
           src="/logo.png"
           alt=""
-          className="h-full absolute w-[170px] left-0 object-cover "
+          className="hidden lg:block h-full absolute w-[170px] left-0 object-cover "
         />
-        <div className="h-full flex justify-center items-center w-full text-lg gap-[20px]">
+        <div className="lg:hidden w-full flex justify-center">
+          <img
+          src="/logo.png"
+          alt=""
+          className="h-full w-[170px] object-cover"
+        />
+        </div>
+        
+        <div className="hidden h-full lg:flex justify-center items-center w-full text-lg gap-[20px]">
           <Link to="/">Home</Link>
           <Link to="/products">Products</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
         </div>
-        <Link className="flex justify-center items-center" to="/cart">
+        <Link className="hidden h-full lg:flex justify-center items-center absolute right-0 " to="/cart">
           <BsCart3 className="text-3xl" />
         </Link>
       </div>
