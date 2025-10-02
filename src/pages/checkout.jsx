3f39f0bc3323f95e9b5669252b59e51104b012bf
NonimUdara <nonimudara123@gmail.com>
2025-10-02 20:41:35 +0530

    update checkout page with mobile respomsiveness.

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -7,7 +7,6 @@ import axios from "axios";
 
 export default function CheckoutPage() {
   const location = useLocation();
-
   const [cart, setCart] = useState(location.state);
   const navigate = useNavigate();
 
@@ -36,7 +35,7 @@ export default function CheckoutPage() {
           quantity: cart[i].quantity,
         });
       }
-      
+
       await axios.post(
         import.meta.env.VITE_API_URL + "/api/orders",
         {
@@ -51,95 +50,93 @@ export default function CheckoutPage() {
       );
 
       toast.success("Order placed successfully");
-
     } catch (error) {
       toast.error("Error fetching products");
       console.log(error);
-      //if error is 400
       if (error.response && error.response.status == 400) {
-
         toast.error(error.response.data.message);
-
       }
     }
   }
 
   return (
-    <div className="w-full h-[calc(100vh-100px)] bg-primary text-secondary flex flex-col pt-[25px] items-center ">
-      <div className="w-[600px] flex flex-col gap-4 ">
-        {cart.map((item, index) => {
-          return (
-            <div
-              className="w-full h-[120px] bg-white flex relative items-center "
-              key={index}
-            >
-              <button
-                className="absolute text-red-500 right-[-40px] font-bold text-2xl rounded-full aspect-square hover:bg-red-500 hover:text-white p-[5px] "
-                onClick={() => {
-                  const newCart = cart;
-                }}
-              >
-                <BiTrash />
-              </button>
-              <img
-                src={item.image}
-                alt=""
-                className="h-full aspect-square object-cover "
-              />
-              <div className="w-[200px] h-full flex flex-col pl-[5px] pt-[10px] ">
-                <h1 className="w-full text-wrap text-xl font-semibold text-secondary ">
-                  {item.name}
-                </h1>
-                <p className="text-sm text-secondary/70 ">{item.productID}</p>
-              </div>
-              <div className="w-[100px] h-full flex flex-col items-center justify-center ">
-                <CiCircleChevUp
-                  className="text-3xl "
-                  onClick={() => {
-                    const newCart = [...cart];
-
-                    newCart[index].quantity += 1;
-
-                    setCart(newCart);
-                  }}
-                />
-                <span className="text-2xl font-semibold ">{item.quantity}</span>
-                <CiCircleChevDown
-                  className="text-3xl "
-                  onClick={() => {
-                    const newCart = [...cart];
-
-                    if (newCart[index].quantity > 1) {
-                      newCart[index].quantity -= 1;
-                    }
-
-                    setCart(newCart);
-                  }}
-                />
-              </div>
-              <div className="w-[180px] h-full flex flex-col ">
-                {item.labledPrice > item.price && (
-                  <span className="text-xl font-semibold w-full line-through text-right pr-[10px] mt-[20px] ">
-                    LKR {item.labledPrice.toFixed(2) * item.quantity}
-                  </span>
-                )}
-                <span className="text-2xl text-accent font-semibold w-full text-right pr-[10px] ">
-                  LKR {item.price.toFixed(2) * item.quantity}
-                </span>
-              </div>
-            </div>
-          );
-        })}
-        <div className="w-full h-[120px] bg-white flex justify-end items-center relative ">
-          <button
-            to="/checkout"
-            onClick={purchaseCart}
-            className="absolute left-0 bg-accent text-white px-6 py-3 ml-[20px] font-semibold "
+    <div className="w-full lg:h-[calc(100vh-100px)] bg-primary text-secondary flex flex-col pt-[25px] items-center">
+      <div className="w-[400px] lg:w-[600px] flex flex-col gap-4">
+        {cart.map((item, index) => (
+          <div
+            key={index}
+            className="w-full h-[300px] lg:h-[120px] bg-white flex flex-col lg:flex-row relative items-center p-3 lg:p-0"
           >
-            order
+            <button
+              className="absolute text-red-500 right-[-40px] font-bold text-2xl rounded-full aspect-square hover:bg-red-500 hover:text-white p-[5px]"
+              onClick={() => {
+                const newCart = [...cart];
+                newCart.splice(index, 1);
+                setCart(newCart);
+              }}
+            >
+              <BiTrash />
+            </button>
+
+            <img
+              src={item.image}
+              alt=""
+              className="h-[100px] lg:h-full aspect-square object-cover"
+            />
+
+            <div className="w-[200px] h-[100px] lg:h-full flex flex-col pl-[5px] pt-[10px]">
+              <h1 className="w-full lg:w-[200px] text-center text-wrap text-xl font-semibold text-secondary">
+                {item.name}
+              </h1>
+              <p className="text-sm text-secondary/70 text-center lg:text-left">
+                {item.productID}
+              </p>
+            </div>
+
+            <div className="w-[100px] h-full flex flex-row lg:flex-col items-center justify-center">
+              <CiCircleChevUp
+                className="text-3xl"
+                onClick={() => {
+                  const newCart = [...cart];
+                  newCart[index].quantity += 1;
+                  setCart(newCart);
+                }}
+              />
+              <span className="text-2xl font-semibold">{item.quantity}</span>
+              <CiCircleChevDown
+                className="text-3xl"
+                onClick={() => {
+                  const newCart = [...cart];
+                  if (newCart[index].quantity > 1) {
+                    newCart[index].quantity -= 1;
+                    setCart(newCart);
+                  }
+                }}
+              />
+            </div>
+
+            <div className="w-full lg:w-[180px] lg:h-full flex flex-row lg:flex-col items-center justify-center">
+              {item.labledPrice > item.price && (
+                <span className="text-xl font-semibold lg:w-full line-through text-center lg:text-right pr-[10px]">
+                  LKR {(item.labledPrice * item.quantity).toFixed(2)}
+                </span>
+              )}
+              <span className="text-2xl text-accent font-semibold lg:w-full text-center lg:text-right pr-[10px]">
+                LKR {(item.price * item.quantity).toFixed(2)}
+              </span>
+            </div>
+          </div>
+        ))}
+
+        <div className="w-full h-[120px] bg-white flex flex-col-reverse lg:flex-row justify-end items-center relative">
+          <button
+            onClick={purchaseCart}
+            className="lg:absolute left-0 bg-accent text-white px-6 py-3 lg:ml-[20px] font-semibold"
+          >
+            Order
           </button>
-          <div className="h-[50px] ">
-            <span className="text-2xl font-semibold w-full text-right pr-[10px] text-accent ">
+          <div className="h-[50px] text-center">
+            <span className="text-2xl font-semibold w-full text-center lg:text-right lg:pr-[10px] text-accent">
               Total : LKR {getTotal().toFixed(2)}
             </span>
           </div>
@@ -148,6 +145,7 @@ export default function CheckoutPage() {
     </div>
   );
 }
+
 
 
 // import { CiCircleChevDown, CiCircleChevUp } from "react-icons/ci";
